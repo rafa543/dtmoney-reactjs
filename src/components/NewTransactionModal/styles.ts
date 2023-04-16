f146e5d8b147b9c73af4bbb006f1914ba311e388
Rafael Araujo <rafa123839@gmail.com>
2023-04-16 15:03:24 -0300

    Cores dos botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,4 @@
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
@@ -57,6 +57,12 @@ export const TransactionTypeContainer = styled.div`
 `
 interface RadioBox  {
    isActive: boolean;
+   activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#e52e4d'
 }
 
 export const RadioBox = styled.button<RadioBox>`
@@ -64,7 +70,9 @@ export const RadioBox = styled.button<RadioBox>`
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: ${(props) => props.isActive ? "#eee" : 'transparent'};
+    background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor]) 
+        : 'transparent'};
     display: flex;
     align-items: center;
     justify-content: center;
